451aa6d5b5b38211e9d9f88880b1a52637338be2
Eddy Ruiz <ruso1097@hotmail.com>
2025-09-08 18:30:25 -0700

    Stop rejecting unauthenticated requests to signup and login paths

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,24 +4,27 @@ import errorCustomizer from '../utils/errors.js';
 
 const authenticator = {
     authenticateRequest: (req, res, next) => {
+            const publicRoutes = ['/login', '/signup']
             const authHeader = req.get('Authorization');
-            if (!authHeader) {
-                throw errorCustomizer.createError(401, "Not authenticated");
-            }
-                const token = authHeader.split(' ')[1];
-                let decodedToken;
-                try {
-                    decodedToken = jwt.verify(token, process.env.SECRET);
-                } catch (err) {
-                    throw errorCustomizer(500, "Internal Error")
+            if(! publicRoutes.includes(req.path)){
+                if (!authHeader) {
+                    throw errorCustomizer.createError(401, 'Not authenticated');
+                }
+                    const token = authHeader.split(' ')[1];
+                    let decodedToken;
+                    try {
+                        decodedToken = jwt.verify(token, process.env.SECRET);
+                    } catch (err) {
+                        throw errorCustomizer(500, 'Internal Error')
+                    }
+
+                if (!decodedToken) {
+                    throw errorCustomizer.createError(401, 'Not authenticated.');
                 }
 
-            if (!decodedToken) {
-                throw errorCustomizer.createError(401, 'Not authenticated.');
-            }
-
-            req.decodedToken = decodedToken;
-            next();
+                req.decodedToken = decodedToken;
+        }
+        next();
       }
 }
 
